fc89cc0e8cc02df534437d8381f66e46fff39536
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-03-24 10:50:47 +0200

    add some styles

--- src/App.vue
+++ src/App.vue
@@ -4,38 +4,17 @@ import ZipCodeFinder from './components/ZipCodeFinder.vue';
 </script>
 
 <template>
-  <div>
-    <h1>Zip Code Finder</h1>
+  <div class="app">
+    <h1 class="app__title">Zip Code Finder</h1>
     <zip-code-finder></zip-code-finder>
   </div>
 </template>
 
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.app__title {
+  text-align: center;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  font-size: 2.5rem;
 }
 </style>
--- src/components/ZipCodeFinder.vue
+++ src/components/ZipCodeFinder.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <input v-model="zipCode" placeholder="Enter ZIP code">
-    <button @click="search">Search</button>
-    <div v-if="address">
+  <div className="zip-code-finder">
+    <input className="zip-code-finder__input" v-model="zipCode" placeholder="Enter ZIP code">
+    <button className="zip-code-finder__button" @click="search">Search</button>
+    <div v-if="address" className="zip-code-finder__address">
       <p>{{ address.places[0]['place name'] }}</p>
       <p>{{ address.places[0].state }}</p>
     </div>
-    <div v-if="errorMessage">
+    <div v-if="errorMessage" className="zip-code-finder__error">
       {{ errorMessage }}
     </div>
   </div>
@@ -37,3 +37,35 @@ export default {
   }
 };
 </script>
+
+<style>
+.zip-code-finder {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.zip-code-finder__input {
+  margin-bottom: 10px;
+  padding: 5px;
+  border: none;
+  border-bottom: 1px solid #ccc;
+}
+
+.zip-code-finder__button {
+  background-color: #007bff;
+  color: white;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 3px;
+}
+
+.zip-code-finder__address {
+  margin-top: 10px;
+}
+
+.zip-code-finder__error {
+  color: red;
+  margin-top: 10px;
+}
+</style>
